12e9e805e371e28110a33dbe5a7bf1d81acd2b0a
Mauricio Rodriguez <mrodriguezv92@gmail.com>
2022-08-02 23:21:27 -0500

    feat: Add search list and layout styles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,5 +4,15 @@ import Handlebars from 'handlebars/dist/handlebars';
 let homeContent = document.getElementById('home-template-content');
 let homeTemplate = document.getElementById('home-template').textContent;
 
-homeTemplate = Handlebars.compile(homeTemplate);
-homeContent.innerHTML = homeTemplate();
+document.addEventListener('DOMContentLoaded', () => {
+  homeTemplate = Handlebars.compile(homeTemplate);
+  homeContent.innerHTML = homeTemplate();
+
+  let searchInput = document.getElementById('search-input');
+  let searchResult = document.getElementById('search-result');
+  searchResult.style.width = `${searchInput.offsetWidth}px`;
+
+  window.addEventListener('resize', () => {
+    searchResult.style.width = `${searchInput.offsetWidth}px`;
+  });
+});

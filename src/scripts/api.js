225d2dcd6a3fc5199f42cedd99fd344058749a92
Mauricio Rodriguez <mrodriguezv92@gmail.com>
2022-08-04 13:10:43 -0500

    feat: Get search list and refactor code

---
+++ src/scripts/api.js
@@ -0,0 +1,17 @@
+/*global process*/
+/*eslint no-undef: "error"*/
+
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.API_URL;
+axios.defaults.headers.common['x-api-key'] = process.env.API_KEY;
+
+/**
+ * Get breed list.
+ * @return {Promise} A axios promise
+ */
+function getBreeds() {
+  return axios.get('breeds');
+}
+
+export { getBreeds };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,64 @@
+import homeTemplate from '../handlebars/home.hbs';
+import listTemplate from '../handlebars/partials/list.hbs';
+import { getBreeds } from './api';
 import '../styles/index.scss';
-import Handlebars from 'handlebars/dist/handlebars';
-
-let homeContent = document.getElementById('home-template-content');
-let homeTemplate = document.getElementById('home-template').textContent;
 
 document.addEventListener('DOMContentLoaded', () => {
-  homeTemplate = Handlebars.compile(homeTemplate);
+  let homeContent = document.getElementById('home-template-content');
   homeContent.innerHTML = homeTemplate();
+  setListBreeds();
+});
 
-  let searchInput = document.getElementById('search-input');
-  let searchResult = document.getElementById('search-result');
-  searchResult.style.width = `${searchInput.offsetWidth}px`;
+/**
+ * Create breed list and add events.
+ */
+function setListBreeds() {
+  const searchInput = document.getElementById('search-input');
+  const listContent = document.getElementById('list-template-content');
+  const searchResult = document.getElementById('list-template-content');
+
+  getBreeds().then((res) => {
+    const data = res.data;
+    listContent.innerHTML = listTemplate({ breeds: data });
+    searchResult.style.width = `${searchInput.offsetWidth}px`;
+    setListItemEvent();
+
+    searchInput.addEventListener('input', (event) => {
+      const filterText = event.target.value;
+      const filterBreeds = data.filter((item) => {
+        return item.name.toLowerCase().match(filterText);
+      });
+
+      listContent.innerHTML = listTemplate({ breeds: filterBreeds });
+      setListItemEvent();
+    });
+  });
 
   window.addEventListener('resize', () => {
     searchResult.style.width = `${searchInput.offsetWidth}px`;
   });
-});
+
+  searchInput.addEventListener('focus', () => {
+    searchResult.classList.remove('hidden');
+  });
+
+  searchInput.addEventListener('blur', (event) => {
+    event.preventDefault();
+    setTimeout(() => {
+      searchResult.classList.add('hidden');
+    }, 200);
+  });
+}
+
+/**
+ * Set event for list items.
+ */
+function setListItemEvent() {
+  const listItems = document.querySelectorAll('.list__item');
+
+  listItems.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      alert(event.target.dataset.id);
+    });
+  });
+}
